308d6941ec0d1e2d18789bdba2bd34bcecfa02b6
marcofcp <marco_fcp85@hotmail.com>
2022-09-20 10:19:55 +0100

    Code comment
    To better understand the code for anyone i just comment as needed.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var submitButton = document.querySelector('#submit-button');
 var box = document.querySelector('#box');
 var boxcontentthanks = document.querySelector('#box-content-thanks');
-var ratings = document.querySelectorAll('.botao-rating')
+var ratings = document.querySelectorAll('.button-rating')
 var ratingValue = document.getElementById('rating-span');
+
+/* Display and Hide content according to submit button */
 
 function submit() {
     box.style.display = "none";
     boxcontentthanks.style.display = "block";
 }
+
+/* Code that runs on each .button-rating and change result span html */
 
 ratings.forEach((rate) => {
     rate.addEventListener("click", () => {
